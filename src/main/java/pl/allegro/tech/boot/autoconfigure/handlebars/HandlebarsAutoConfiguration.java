4e3fee9aa1055ca19e267e9b17117b2ad6a5ff84
Rafal Zukowski <rafal.zukowski@allegrogroup.com>
2015-08-07 11:17:38 +0200

    possibility to inject custom view resolver and cache implementation

--- src/main/java/pl/allegro/tech/boot/autoconfigure/handlebars/HandlebarsAutoConfiguration.java
+++ src/main/java/pl/allegro/tech/boot/autoconfigure/handlebars/HandlebarsAutoConfiguration.java
@@ -2,9 +2,11 @@ package pl.allegro.tech.boot.autoconfigure.handlebars;
 
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.cache.GuavaTemplateCache;
+import com.github.jknack.handlebars.cache.TemplateCache;
 import com.github.jknack.handlebars.io.TemplateSource;
 import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +27,7 @@ public class HandlebarsAutoConfiguration {
         private HandlebarsProperties handlebars;
 
         @Bean
+        @ConditionalOnMissingBean
         public HandlebarsViewResolver handlebarsViewResolver() {
             HandlebarsViewResolver handlebarsViewResolver = new HandlebarsViewResolver();
             handlebarsViewResolver.setFailOnMissingFile(false);
@@ -38,11 +41,21 @@ public class HandlebarsAutoConfiguration {
         @Autowired
         private HandlebarsViewResolver handlebarsViewResolver;
 
+        @Autowired
+        private TemplateCache templateCache;
+
         @PostConstruct
         public void setCachingStrategy() {
             if (handlebarsViewResolver.isCache()) {
-                handlebarsViewResolver.getHandlebars().with(new GuavaTemplateCache(newBuilder().<TemplateSource, Template>build()));
+                handlebarsViewResolver.getHandlebars().with(templateCache);
             }
         }
+
+        @Bean
+        @ConditionalOnMissingBean
+        public TemplateCache templateCache() {
+            return new GuavaTemplateCache(newBuilder().<TemplateSource, Template>build());
+        }
+
     }
 }
